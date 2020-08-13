51fc3d8d4885c15f19e5e84085f5861690c62a56
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2020-08-13 11:53:36 +0700

    [MIS]to_test_sale: test dependencies

---
+++ to_test_sale/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "test",
+
+    'summary': """
+""",
+
+    'description': """
+    """,
+    'author': "nhatnm0612",
+    'website': "",
+    'category': 'Odoo Apps',
+    'version': '0.8',
+    'depends': ["to_test", "base"],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'price': 4.9,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
+
