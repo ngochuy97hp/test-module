b5aa19667771cca6220bf372e7db99a4e7692e5f
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2020-08-28 17:01:56 +0700

    1

---
+++ new_themes/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': "themes",
+
+    'summary': """
+""",
+
+    'description': """
+    TEST MODULE
+    """,
+    'author': "nhatnm0612",
+    'website': "",
+    'category': 'Backend',
+    'version': '0.8',
+    'depends': [],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'price': 4.9,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
+
