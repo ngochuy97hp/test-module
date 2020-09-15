16945cdc848e1a1ff32580263c353a50fce997e5
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2020-09-15 09:22:45 +0700

    test update git branch

---
+++ to_test_template/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "test sale template",
+
+    'summary': """
+""",
+
+    'description': """
+    """,
+    'author': "nhatnm0612",
+    'website': "",
+    'category': 'Odoo Apps',
+    'version': '0.8',
+    'depends': ["to_test_sale"],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'price': 4.9,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
+
