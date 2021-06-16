9b9883507856f32f245bb9ca6d8691c6b0c0a37e
nhatnm0612 <nhatnguyenminh061289@gmail.com>
2021-06-16 09:39:11 +0700

    fix shit on model

--- to_test/models/model.py
+++ to_test/models/model.py
@@ -3,14 +3,13 @@ from odoo import models, fields
 
 class LibraryBook(models.Model):
     _name = 'library.book'
-    name = fields.Char('Title', required=True)
+    name = fields.Char('Title', required=True, help="name of library book")
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many('res.partner', string='Authors')
-    state = fields.Selection(
-                            [('draft', 'Not Available'),
+    state = fields.Selection([('draft', 'Not Available'),
                             ('available', 'Available'),
                             ('lost', 'Lost')],
-                            'State', default="draft")
+                            'State', default="draft", required=True)
     color = fields.Integer()
 
     def make_available(self):
